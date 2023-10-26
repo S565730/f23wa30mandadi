57ebdf680c1d182977fbf3a42462c16287ee3efb
S565730 <S565730@nwmissouri.edu>
2023-10-25 20:00:14 -0500

    Update mydata.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Rachana sri Mandadi' });
+  res.render('mydata', { title: 'Rachana sri Mandadi' });
 });
 
 module.exports = router;
